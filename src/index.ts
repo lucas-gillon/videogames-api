5aae6ef1d3d2834bb2169bd033156a54dbb1d2ac
Lucas Gillon <lucas.gillon98@gmail.com>
2022-02-21 15:24:04 +0100

    replaced request with fetch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-/* eslint-disable prettier/prettier */
-import request from "@fewlines-education/request";
 import express from "express";
 import nunjucks from "nunjucks";
+import fetch from "node-fetch";
 
 const app = express();
 
@@ -19,100 +18,82 @@ app.get("/", (req, response) => {
 
 app.get("/platformPages", (req, response) => {
   const pageNumber = parseInt(String(req.query.page));
-  request(`http://videogame-api.fly.dev/platforms/?page=${pageNumber}`, (error, body) => {
-    if (error) {
-      throw error;
-    }
-    const platformsList = JSON.parse(body);
-
-    response.render("listOfPlatforms", {
-      pageNumber,
-      platforms: platformsList.platforms,
-    });
-  });
+  fetch(`http://videogame-api.fly.dev/platforms/?page=${pageNumber}`)
+    .then((response) => response.json())
+    .then((platformsList) =>
+      response.render("listOfPlatforms", {
+        pageNumber,
+        platforms: platformsList.platforms,
+      }),
+    );
 });
 
 app.get("/randomGame", (req, response) => {
-  request(`http://videogame-api.fly.dev/games`, (error, body) => {
-    const data = JSON.parse(body);
-
-    function entierAleatoire(min: number, max: number): number {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    const entier = entierAleatoire(0, 19);
-    const routeSlug = data.games[entier].slug;
-
-    request(`http://videogame-api.fly.dev/games/slug/${routeSlug}`, (error, body) => {
-      if (error) {
-        throw error;
-      }
-      const game = JSON.parse(body);
-
-      response.render("random-game", {
-        game,
-        gamePlatforms: game.games_platforms,
-        gameGenres: game.games_genres,
-        screenshots: game.game_screenshots,
-      });
+  function entierAleatoire(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  const entier = entierAleatoire(0, 19);
+  fetch(`http://videogame-api.fly.dev/games`)
+    .then((response) => response.json())
+    .then((data) => {
+      const routeSlug = data.games[entier].slug;
+      fetch(`http://videogame-api.fly.dev/games/slug/${routeSlug}`)
+        .then((response) => response.json())
+        .then((game) =>
+          response.render("random-game", {
+            game,
+            gamePlatforms: game.games_platforms,
+            gameGenres: game.games_genres,
+            screenshots: game.game_screenshots,
+          }),
+        );
     });
-  });
 });
 
 app.get("/platform/:slug", (req, response) => {
   const platformSlug = req.params.slug;
   const pageNumber = parseInt(String(req.query.page));
-  request(`http://videogame-api.fly.dev/platforms/slug/${platformSlug}`, (error, body) => {
-    if (error) {
-      throw error;
-    }
-    const platform = JSON.parse(body);
-    const platformID = platform.id;
-    request(`http://videogame-api.fly.dev/games/platforms/${platformID}?page=${pageNumber}`, (error, body) => {
-      if (error) {
-        throw error;
-      }
-      const data = JSON.parse(body);
-
-      response.render("gamesNames", {
-        pageNumber,
-        platformSlug,
-        games: data.games,
-      });
+  fetch(`http://videogame-api.fly.dev/platforms/slug/${platformSlug}`)
+    .then((response) => response.json())
+    .then((platform) => {
+      const platformID = platform.id;
+      fetch(`http://videogame-api.fly.dev/games/platforms/${platformID}?page=${pageNumber}`)
+        .then((response) => response.json())
+        .then((data) => {
+          response.render("gamesNames", {
+            pageNumber,
+            platformSlug,
+            games: data.games,
+          });
+        });
     });
-  });
 });
 
 app.get("/games/:slug", (req, response) => {
   const routeSlug = req.params.slug;
-  request(`http://videogame-api.fly.dev/games/slug/${routeSlug}`, (error, body) => {
-    if (error) {
-      throw error;
-    }
-    const game = JSON.parse(body);
-
-    response.render("gameInfos", {
-      game,
-      gamePlatforms: game.games_platforms,
-      gameGenres: game.games_genres,
-      screenshots: game.game_screenshots,
-    });
-  });
+  fetch(`http://videogame-api.fly.dev/games/slug/${routeSlug}`)
+    .then((response) => response.json())
+    .then((game) =>
+      response.render("gameInfos", {
+        game,
+        gamePlatforms: game.games_platforms,
+        gameGenres: game.games_genres,
+        screenshots: game.game_screenshots,
+      }),
+    );
 });
 
 app.get("/allGames", (req, response) => {
   const pageNumber = parseInt(String(req.query.page));
-  request(`http://videogame-api.fly.dev/games?page=${pageNumber}`, (error, body) => {
-    if (error) {
-      throw error;
-    }
-    const gamesList = JSON.parse(body);
-
-    response.render("all-games", {
-      pageNumber,
-      games: gamesList.games,
-    });
-  });
-})
+  fetch(`http://videogame-api.fly.dev/games?page=${pageNumber}`)
+    .then((response) => response.json())
+    .then((gamesList) =>
+      response.render("all-games", {
+        pageNumber,
+        games: gamesList.games,
+      }),
+    );
+});
 
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
